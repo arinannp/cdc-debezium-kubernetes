7626c7830df1ed7d57e54508219e5c9ffd0d469f
arinan <arinan.putra@point-star.com>
2022-12-08 21:54:28 +0700

    Added docs to run this project

--- run.sh
+++ run.sh
@@ -1,45 +1,44 @@
-"""
-Be aware that deploying Cluster Operator requires a Kubernetes user account that has permission to create:
-CustomResourceDefinitions, ClusterRoles and ClusterRoleBindings.
-"""
-kubectl create clusterrolebinding strimzi-cluster-operator-namespaced --clusterrole=strimzi-cluster-operator-namespaced --serviceaccount kafka:strimzi-cluster-operator
-kubectl create clusterrolebinding strimzi-cluster-operator-entity-operator-delegation --clusterrole=strimzi-entity-operator --serviceaccount kafka:strimzi-cluster-operator
-kubectl create clusterrolebinding strimzi-cluster-operator-topic-operator-delegation --clusterrole=strimzi-topic-operator --serviceaccount kafka:strimzi-cluster-operator
+# requirement - docker, minikube, kubernetes
+
+kubectl create ns my-kafka-project
+kubectl create ns my-postgres-project
+kubectl create ns my-beam-project
 
 
-kubectl run kafka-producer -ti --image=strimzi/kafka:0.20.0-rc1-kafka-2.6.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list my-cluster-kafka-kafka-bootstrap.my-kafka-project:9092 --topic my-topic
-kubectl run kafka-consumer -ti --image=strimzi/kafka:0.20.0-rc1-kafka-2.6.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-kafka-bootstrap.my-kafka-project:9092 --topic my-topic --from-beginning
+kubectl create -f strimzi-0.31.1/cluster-operator/020-RoleBinding-strimzi-cluster-operator.yaml -n my-kafka-project
+kubectl create -f strimzi-0.31.1/cluster-operator/031-RoleBinding-strimzi-cluster-operator-entity-operator-delegation.yaml -n my-kafka-project
+
+kubectl create -f strimzi-0.31.1/cluster-operator/ -n my-kafka-project
+
+kubectl apply -f kubernetes/kafka-myproject-kafkacluster.yaml
+kubectl wait kafka/my-cluster-kafka --for=condition=Ready --timeout=300s -n my-kafka-project
+
+
+# optional creating a kafka topic for testing purpose
+kubectl apply -f kubernetes/kafka-myproject-kafkatopic.yaml
+kubectl wait kafkatopic/my-topic-testing --for=condition=Ready --timeout=300s -n my-kafka-project
+kubectl run kafka-producer -ti --image=strimzi/kafka:0.20.0-rc1-kafka-2.6.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --broker-list my-cluster-kafka-kafka-bootstrap.my-kafka-project:9092 --topic my-topic-testing
+kubectl run kafka-consumer -ti --image=strimzi/kafka:0.20.0-rc1-kafka-2.6.0 --rm=true --restart=Never -- bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-kafka-bootstrap.my-kafka-project:9092 --topic my-topic-testing --from-beginning
 kubectl run kafka-topiclist -it --image=strimzi/kafka:0.20.0-rc1-kafka-2.6.0 --rm=true --restart=Never -- bin/kafka-topics.sh --bootstrap-server my-cluster-kafka-kafka-bootstrap.my-kafka-project:9092 --list
 
 
+kubectl apply -f kubernetes/kafka-myproject-kafkaconnect.yaml -n my-kafka-project
+kubectl wait kafkaconnect/my-cluster-kafkaconnect-dbz --for=condition=Ready --timeout=300s -n my-kafka-project
 
-INSERT INTO person_identity (login_date, first_name, last_name, address, active) VALUES ('2022-11-10', 'Arinan', 'Najah', 'Semarang, IND', true);
+kubectl apply -f kubernetes/kafka-myproject-postgres.yaml -n my-postgres-project
+kubectl wait deployment/my-postgresdb --for=condition=Ready --timeout=300s -n my-postgres-project
 
-Changes Data Capture
-PostgreSQL -> Debezium -> Kafka
+kubectl apply -f kubernetes/kafka-myproject-debezium.yaml -n my-kafka-project
+kubectl wait kafkaconnector/my-connector-dbz --for=condition=Ready --timeout=300s -n my-kafka-project
 
-kubectl create ns kafka
-kubectl create ns my-kafka-project
+kubectl apply -f kubernetes/kafka-myproject-pythonkafka.yaml -n my-beam-project
+kubectl wait deployment/my-consumerbeam --for=condition=Ready --timeout=300s -n my-beam-project
 
-kubectl apply -f kafka-myproject-kafkacluster.yaml -n my-kafka-project
-kubectl apply -f kafka-myproject-kafkatopic.yaml -n my-kafka-project
 
-kubectl create secret docker-registry mydockercred --docker-server=https://index.docker.io/v1/ --docker-username=arinannp --docker-password=paridenajah --docker-email=arinannp@gmail.com -n my-kafka-project
-
-Windows Powershell => Get-FileHash file-name.tar.gz -Algorithm SHA512 | Format-List
-Windows Powershell => Get-FileHash file-name.jar -Algorithm SHA512 | Format-List
-
-kubectl apply -f kafka-myproject-kafkaconnect.yaml -n my-kafka-project
-
-kubectl create ns my-postgres-project
-# kubectl create configmap sql-command --from-file=initdb.sql -n my-postgres-project
-
-docker build -t arinannp/postgres-13:latest Postgres/.
-docker push arinannp/postgres-13:latest
-kubectl apply -f kafka-myproject-postgres.yaml -n my-postgres-project
-
+# verify that the cdc process works
 kubectl get pod -n my-postgres-project
-kubectl exec -it my-postgresdb-yourgeneratedpod -n my-postgres-project -- psql -U debezium -d debezium_db
+kubectl exec -it my-postgresdb-yourhashpod -n my-postgres-project -- psql -U debezium -d debezium_db
 
-kubectl apply -f kafka-myproject-debezium.yaml -n my-kafka-project
-kubectl get kafkatopic -A
+kubectl get pod -n my-beam-project
+kubectl exec -it my-beam-project-yourhashpod -n my-beam-project -- bin/sh
+kubectl logs -f my-beam-project-yourhashpod -n my-beam-project
